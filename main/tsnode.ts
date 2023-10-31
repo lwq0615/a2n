4756346df74e2f9cf815b11192ab665394854f5f
liwq <liwq@qilongnet.com>
2023-10-31 10:49:42 +0800

    env

---
+++ main/tsnode.ts
@@ -0,0 +1,45 @@
+import { start as listen } from "@/index"
+const config = require("../a2n.config")
+const fs = require('fs')
+const path = require('path')
+
+/**
+ * 扫描目录下的Controll
+ */
+async function compScan(dirPath: string) {
+  const files = fs.readdirSync(dirPath)
+  if (!files || !files.length) {
+    return
+  }
+  for (const file of files) {
+    const filePath = dirPath + "\\" + file
+    const stat = fs.statSync(filePath)
+    if (stat.isDirectory()) {
+      compScan(filePath)
+    } else {
+      console.log(`scan file '${filePath}'`);
+      await import(filePath)
+    }
+  }
+
+}
+
+
+/**
+ * 开启服务
+ */
+async function start() {
+  let compDirPath = config.componentScan || 'src'
+  console.log('scan components in folder ' + path.resolve(process.cwd(), compDirPath));
+  await compScan(path.resolve(process.cwd(), compDirPath))
+  listen({
+    config,
+    callback: () => {
+      console.log("======================success======================")
+      console.log("          server was start in port: " + config.port || 8080)
+      console.log("===================================================")
+    }
+  })
+}
+
+start()
