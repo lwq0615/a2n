fd9058689fdba93a79415bac5e3d781425f96e43
liweiqiang <liweiqiang@taqu.cn>
2024-10-30 18:23:36 +0800

    update

--- core/index.ts
+++ core/index.ts
@@ -1,19 +1,44 @@
 export {
-  Autowired, Bean, BeanClass,
-  BeanInstance, BeanScope, Config, PostConstruct, Scope, Service, getBean,
-  getBeans, getState
+  Autowired,
+  Bean,
+  BeanClass,
+  BeanInstance,
+  BeanScope,
+  Config,
+  PostConstruct,
+  Scope,
+  Service,
+  getBean,
+  getBeans,
+  getState
 } from '@core/ioc';
 
 export {
   After,
-  Around, AroundInterceptor, Aspect, Before, ErrHandler, Interceptor
+  Around,
+  AroundInterceptor,
+  Aspect,
+  Before,
+  ErrHandler,
+  Interceptor
 } from '@core/aop';
 
-export {
-  ApiExport, Body, Control, Delete, Get,
-  Post, Put, Query, Req, RequestMapping, Res, app, close, start
-} from '@core/control';
-
 export { getConfig, setConfig } from '@core/config';
+export {
+  ApiExport,
+  Body,
+  Control,
+  Delete,
+  Get,
+  Post,
+  Put,
+  Query,
+  Req,
+  RequestMapping,
+  Res,
+  app,
+  close,
+  start
+} from '@core/control';
 
 export { Request, Response } from 'express';
